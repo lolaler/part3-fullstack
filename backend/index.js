2534cf70156ba70ace07fe79ebbfe32b09edd91d
Lola <lola.lerche@gmail.com>
2024-09-26 13:08:22 +0300

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const app = express()
 require('dotenv').config()
 const Person = require('./models/person.js')
 const cors = require('cors')
-const password = process.argv[2]
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -13,102 +12,75 @@ app.use(express.static('dist'))
 app.use(cors())
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-  
-    next(error)
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-
-
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  next(error)
+}
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id)
-        .then(person => {
-            console.log(person)
-            if (person) {
-                response.json(person)
-            } else {
-                console.log('person not found')
-                response.status(404).end()
-            }
-        })
-        .catch(error => {
-            console.log(error)
-            response.status(400).send({ error: 'malformed id' })
-        })
+  Person.findById(request.params.id)
+    .then(person => {
+      console.log(person)
+      if (person) {
+        response.json(person)
+      } else {
+        console.log('person not found')
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformed id' })
+    })
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
+  const person = new Person ({
+    name: body.name,
+    number: body.number
+  })
 
-    const person = new Person ({
-        name: body.name,
-        number: body.number
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
     })
-
-    person.save()
-        .then(savedPerson => {
-            response.json(savedPerson)
-        })
-        .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findById(request.params.id)
+    .then(() => {
+      response.status(204).end()
     })
+    .catch(error => next(error))
+})
 
 app.get('/info', (request, response) => {
-    const currentDate = new Date()
-    Person.find({}).then(result => {
-        const amtPeople = result.length
-        const data = (`Phonebook has info for ${amtPeople} people. ${currentDate}`)
-        response.json(data)
-    })
+  const currentDate = new Date()
+  Person.find({}).then(result => {
+    const amtPeople = result.length
+    const data = (`Phonebook has info for ${amtPeople} people. ${currentDate}`)
+    response.json(data)
+  })
 })
 
 app.use(unknownEndpoint)
@@ -116,5 +88,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- backend/models/person.js
+++ backend/models/person.js
@@ -7,7 +7,7 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch(error => {
@@ -18,7 +18,7 @@ const personSchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
-    required: true},
+    required: true },
   number: {
     type: String,
     minLength: 8,
